f896acdff59e889bf0197a96e31f28b9d1d5f8c8
luis@luis <correia.ops@gmail.com>
2026-02-03 08:51:47 -0300

    lint fail

--- codebase/rdicidr-0.1.0/src/App.test.js
+++ codebase/rdicidr-0.1.0/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/CIDR Calculator/i);
+  const linkElement = screen.getByText(/ANOTHER TST/i);
   expect(linkElement).toBeInTheDocument();
 });
